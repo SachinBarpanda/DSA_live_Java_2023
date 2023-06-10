20d211763c41d11752a90a47bba5fcbf0c060e8a
Sachin <sachinongithub@gmail.com>
2023-06-10 18:17:47 +0530

    Lec 16 codes added

---
+++ Lec16/Array_Disp_Time_Complex.java
@@ -0,0 +1,24 @@
+package Lec16;
+
+import java.util.Scanner;
+
+public class Array_Disp_Time_Complex {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int [] marks = new int[n];
+        marks[0] = -1;
+        marks[1] = 50;
+        marks[2] = 20;
+        marks[3] = 30;
+        marks[4] = 100;
+//        int arr1[] = new int[5];
+        Display(marks);
+    }
+    public static void Display(int[] arr){
+        for(int i = 0 ; i < arr.length ; i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
---
+++ Lec16/Constant_Time.java
@@ -0,0 +1,11 @@
+package Lec16;
+
+public class Constant_Time {
+    public static void main(String[] args) {
+        System.out.println("Kichi Gute");
+        System.out.println("Au kichi gute ");
+        System.out.println("Au kichi gute ");
+        System.out.println("Au kichi gute ");
+        System.out.println("Au kichi gute ");
+    }
+}
