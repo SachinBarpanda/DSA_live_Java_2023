aeb75402b131acbb223ffedc41c347c5ff1cf0e6
Sachin <sachinongithub@gmail.com>
2023-06-23 18:38:14 +0530

    Lec 25 codes added

--- Lec24/Trapping_Rain_Water.java
+++ Lec24/Trapping_Rain_Water.java
@@ -2,25 +2,38 @@ package Lec24;
 
 public class Trapping_Rain_Water {
     public static void main(String[] args) {
-        int [] arr = {0,1,0,2,1,0,1,3,2,1,2,1};
+        int [] arr = {1,8,6,2,5,4,8,3,7};
         System.out.println(trapping(arr));
     }
     public static int trapping(int [] arr){
-        int[] left = new int[arr.length];
-        int[] right = new int[arr.length];
-        left[0] = arr[0];
         int sum = 0;
-        right[arr.length-1] = arr[arr.length-1];
-        for (int i = 1; i < arr.length; i++) {
-            left[i] = Math.max(left[i-1],arr[i]);
-        }
-        for (int i = arr.length-2; i >=0  ; i--) {
-            right[i] = Math.max(right[i+1],arr[i]);
-        }
-
-        for (int i = 0; i < arr.length; i++) {
-            sum+=Math.min(left[i] , right[i] )-arr[i];
+        for (int i = 0, j = arr.length-1; i<j ;) {
+            sum = Math.max(sum, (j-i)*(Math.min(arr[j],arr[i])));
+            if(arr[i]<arr[j]){
+                i++;
+            }else{
+                j--;
+            }
         }
         return sum;
     }
+
+//    public static int trapping(int [] arr){
+//        int[] left = new int[arr.length];
+//        int[] right = new int[arr.length];
+//        left[0] = arr[0];
+//        int sum = 0;
+//        right[arr.length-1] = arr[arr.length-1];
+//        for (int i = 1; i < arr.length; i++) {
+//            left[i] = Math.max(left[i-1],arr[i]);
+//        }
+//        for (int i = arr.length-2; i >=0  ; i--) {
+//            right[i] = Math.max(right[i+1],arr[i]);
+//        }
+//
+//        for (int i = 0; i < arr.length; i++) {
+//            sum+=Math.min(left[i] , right[i] )-arr[i];
+//        }
+//        return sum;
+//    }
 }
