fadb27e2e2a9098c0b75669d586348894bfc8a79
Sachin <sachinongithub@gmail.com>
2023-06-29 19:37:18 +0530

    Lec 26 codes added

---
+++ Lec26/Array_2d_HardCode.java
@@ -0,0 +1,16 @@
+package Lec26;
+
+import java.util.Scanner;
+
+public class Array_2d_HardCode {
+    public static void main(String[] args) {
+//        int[][] arr = new int[3][4];
+        int[][] arr = {{1, 2, 3, 4},
+                        {5, 6, 7, 8},
+                        {9, 10, 11, 12}};
+        int[][] other = arr;
+        System.out.println(arr);
+        System.out.println(other);
+
+    }
+}
