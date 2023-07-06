ef4e6d34bdee6f9bb66f100ede6801d1b0bbb577
Sachin <sachinongithub@gmail.com>
2023-07-06 22:54:58 +0530

    Lec 31 lectures added

---
+++ Lec30/print_Substring.java
@@ -0,0 +1,17 @@
+package Lec30;
+
+public class print_Substring {
+    public static void main(String[] args) {
+        String str = "Inception";
+        printStr(str);
+    }
+    public static void printStr(String str){
+        for (int i = 0; i < str.length(); i++) {
+            for (int j = i+1; j <= str.length(); j++) {
+                String s = str.substring(i,j);
+                System.out.println(s);
+            }
+        }
+    }
+
+}
---
+++ Lec31/Subarray_Product_Less_Than_K.java
@@ -0,0 +1,30 @@
+package Lec31;
+
+public class Subarray_Product_Less_Than_K {
+    public static void main(String[] args) {
+        int [] arr = {1,2,4,3,5};
+        int k = 10;
+        System.out.println(subarrProd(arr,k));
+    }
+    public static int subarrProd(int[] arr , int k){
+        int si = 0;
+        int ei = 0;
+        int prod = 1;
+        int ans = 0;
+        while(ei<arr.length){
+//            grow kara
+            prod = prod * arr[ei];
+
+//            shrink kara
+            while(ei>=si && prod>=k){
+                prod = prod / arr[si];
+                si++;
+            }
+//            ans save karma
+
+            ans = ans + ei-si+1;
+            ei++;
+        }
+        return ans;
+    }
+}
